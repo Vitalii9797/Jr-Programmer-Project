8d71a97a017dea4b6848fc96e1bbafcdfea34b27
Vitalii9797 <105458621+Vitalii9797@users.noreply.github.com>
2023-03-26 00:52:06 +0200

    Jr programmer
    Implement Data persistence between scenes

---
+++ Assets/Scripts/MainManager.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainManager : MonoBehaviour
+{
+    public static MainManager instance;
+
+    public Color TeamColor;
+
+    private void Awake()
+    {
+        if(instance != null)
+        {
+            Destroy(gameObject);
+        }
+
+        instance = this;
+        DontDestroyOnLoad(gameObject);
+    }
+}
